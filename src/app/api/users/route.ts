01adc9048d4d845775aa8e5c2b831daf7f7c40a2
Toshiki2968 <toshiki2968@gmail.com>
2025-08-03 02:13:43 +0900

    prismaでgetとpostをできるようにした

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,21 @@
 // Notice from where NextResponse is imported:
 import { NextResponse } from "next/server";
+import { PrismaClient } from "../../../../generated/prisma_client";
 
 export async function GET() {
-  return NextResponse.json({ message: "ユーザーを取得しました" });
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  return NextResponse.json({ message: "ユーザーを取得しました", users });
+}
+
+export async function POST() {
+  const prisma = new PrismaClient();
+  const user = await prisma.user.create({
+    data: {
+      email: "toshiki@prisma.io",
+      name: "Toshiki Prisma",
+    },
+  });
+  console.log("User created:", user);
+  return NextResponse.json({ user });
 }
