ecfc57752243627428678d3d0ac5aadf7411a682
Toshiki2968 <toshiki2968@gmail.com>
2025-09-14 23:28:13 +0900

    アニメ管理画面を修正

--- src/app/admin/anime/page.tsx
+++ src/app/admin/anime/page.tsx
@@ -54,7 +54,6 @@ export default function AnimeAdminPage() {
     }
   };
 
-  console.log("anime:", anime);
   // TODO: アニメ編集
   // TODO: アニメ削除
 
@@ -75,39 +74,98 @@ export default function AnimeAdminPage() {
         <button type="submit">アニメを登録</button>
       </form>
 
-      {/* TODO: 一覧画面作成 */}
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>作成日</th>
-            <th>更新日</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(animeList) && animeList.length > 0 ? (
-            animeList.map((anime: Anime) => (
-              <tr key={anime.id}>
-                <td>{anime.id}</td>
-                <td>{anime.title}</td>
-                <td>{anime.createdAt}</td>
-                <td>{anime.updatedAt}</td>
-                <td>
-                  <button>編集</button>
-                </td>
-                <td>
-                  <button>削除</button>
-                </td>
+      <div className="p-10">
+        <div className="flex justify-end ">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ">
+            新規登録
+          </button>
+        </div>
+
+        {/* TODO: 一覧画面作成 */}
+        <div>
+          <table className="w-full">
+            <thead>
+              <tr className="bg-gray-50">
+                <th className="text-left p-5 text-gray-900">ID</th>
+                <th className="text-left p-5 text-gray-900">タイトル</th>
+                <th className="text-left p-5 text-gray-900">作成日</th>
+                <th className="text-left p-5 text-gray-900">更新日</th>
+                <th className="text-left p-5"></th>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={6}>アニメが見つかりませんでした。</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {Array.isArray(animeList) && animeList.length > 0 ? (
+                animeList.map((anime: Anime) => (
+                  <tr key={anime.id}>
+                    <td className="p-5 text-gray-900">{anime.id}</td>
+                    <td className="p-5 text-gray-900">{anime.title}</td>
+                    <td className="p-5 text-gray-900">{anime.createdAt}</td>
+                    <td className="p-5 text-gray-900">{anime.updatedAt}</td>
+                    <td>
+                      <button title="編集" className="p-5">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          className="lucide lucide-square-pen-icon lucide-square-pen"
+                          color="blue"
+                        >
+                          <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                          <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
+                        </svg>
+                      </button>
+                      <button title="削除">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          className="lucide lucide-trash2-icon lucide-trash-2"
+                          color="red"
+                        >
+                          <path d="M10 11v6" />
+                          <path d="M14 11v6" />
+                          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                          <path d="M3 6h18" />
+                          <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={6}>アニメが見つかりませんでした。</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
+
+// const EditDialog = () => {
+//   return (
+//     <div>
+//       <h2>アニメ編集</h2>
+//       <form>
+//         <input type="text" placeholder="アニメタイトル" />
+//         <button type="submit">保存</button>
+//         <button type="button">キャンセル</button>
+//       </form>
+//     </div>
+//   );
+// };
